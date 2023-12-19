10eb892233fa7cc151a9ba1adbb48312703e2c35
claudio.massarotto <claudio.massarotto@hinto.com>
2023-12-19 19:06:20 +0100

    Changes made by Alessio... !cool!

--- src/App.js
+++ src/App.js
@@ -12,45 +12,31 @@ import { useEffect, useState } from 'react';
 function App() {
   const [toggleSanta, setToggleSanta] = useState(false);
   const [question2answer, setQuestion2answer] = useState(4);
-  const [currentPage, setCurrentPage] = useState(null);
   const goSantaFn = () => {
-    setToggleSanta(!toggleSanta);
-  };
-  const handleQuestionsLogic = () => {
-    let toReturn;
-    switch(question2answer){
-      case 0: toReturn = <BestActor handler={nextStep} />; break;
-      case 1: toReturn = <BestActor handler={nextStep} />; break;
-      case 2: toReturn = <BestActivity handler={nextStep} />; break;
-      case 3: toReturn = <BestTime handler={nextStep} />; break;
-      case 4: toReturn = <FavColor handler={nextStep} />; break;
-      default: toReturn = <FavColor handler={nextStep} />;
-    }
-    let number = question2answer;
-    return { page: toReturn, number: --number };
+    setToggleSanta(state=>!state);
+
+    setTimeout(()=>{
+      setToggleSanta(state=>!state);
+    },4000)
   };
 
   const nextStep = () => {
     console.log('next step');
-    const {page, number} = handleQuestionsLogic();
-    console.log(page, number);
-    setCurrentPage(page);
-    setQuestion2answer(number);
+    setQuestion2answer(state=>state-1);
   };
-
-  useEffect(() => {
-    const {page, number} = handleQuestionsLogic();
-    setCurrentPage(page);
-    setQuestion2answer(number);
-  }, []);
 
   return (
     <div className="App">
         <header className="App-header">
           <img src={ghirlanda} className="ghirlanda" alt="xmas-decoration" />
           <label className='app-title'>Cosa riceverai per regalo?</label>
-
-          {currentPage}
+          <>
+            {question2answer === 0 && <BestActor handler={nextStep} />}
+            {question2answer === 1 && <BestActor handler={nextStep} />}
+            {question2answer === 2 && <BestActivity handler={nextStep} />}
+            {question2answer === 3 && <BestTime handler={nextStep} />}
+            {question2answer === 4 && <FavColor handler={nextStep} />}
+          </>
           <Button onClick={goSantaFn}>Test</Button>
           <img src={santa} className={toggleSanta ? "walking-claus go-santa": "walking-claus"} alt="xmas-decoration" />
         </header>
